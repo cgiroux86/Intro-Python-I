edc2e9d616debda796b8118a409ac9ba4479d4bf
Chris Giroux <chrisgiroux@MacBook-Pro.fios-router.home>
2020-07-06 15:32:28 -0400

    first half done

--- src/05_lists.py
+++ src/05_lists.py
@@ -1,3 +1,5 @@
+
+from functools import reduce
 # For the exercise, look up the methods and functions that are available for use
 # with Python lists.
 
@@ -8,22 +10,28 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
+
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+x.extend(val for val in y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(len(x)-1, 99)
 print(x)
-
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+print(reduce(lambda a, b: a + b*1000, x, 0))
